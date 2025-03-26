b18ab14b55e905ce4f1c7d13bf74e7a2d7464d5e
ITk21006 <martins.zudans@va.lv>
2025-03-27 01:22:46 +0200

    file location fix

--- AoC_D9.py
+++ AoC_D9.py
@@ -1,46 +1,41 @@
-﻿with open("E:\Code\input.txt") as f:
+﻿with open("E:\\repos\\dummy-code\\input.txt") as f:
     ns = list(map(int, f.read().strip()))
 
 # Part 1
 disk = []  
 for i, n in enumerate(ns):
-    disk += [None if i % 2 else i // 2] * n # Add `n` elements to the disk, alt between `None` and `i // 2`
+    disk += [None if i % 2 else i // 2] * n 
 
-head = ns[0]  # Set initial position
+head = ns[0]  
 while head < len(disk):
-    if disk[head]:  # If `None`, move forward
+    if disk[head]: 
         head += 1
-    elif num := disk.pop():  # If `None`, pop last element from disk
-        disk[head] = num  # Place popped element at the current position
+    elif num := disk.pop():  
+        disk[head] = num  
 
 print(sum(i * n for i, n in enumerate(disk)))
 
 # Part 2
 blocks = []  
-head = 0  # Set initial position
+head = 0  
 for i, n in enumerate(ns):
-    if not i % 2:  # Only process even numbers
-        blocks.append((i // 2, head, head + n)) # Add block to the list (ID, start, end)
-    head += n  # Move head forward by `n`
+    if not i % 2:  
+        blocks.append((i // 2, head, head + n))
+    head += n  
 
-# Move blocks in reverse order of their IDs
 for to_move in range(i // 2, -1, -1):
-    # Find the block with the current ID to move
     block = next(b for b in blocks if b[0] == to_move)
     _, start, end = block
-    space_needed = end - start  # Calculate the space needed for the block
+    space_needed = end - start  
 
-    # Find a suitable position to move the block
     for i, ((_, _, end1), (_, start2, _)) in enumerate(zip(blocks, blocks[1:])):
-        if end1 == end:  # If the block is already at the end, stop
+        if end1 == end:  
             break
-        if start2 - end1 >= space_needed:  # Check if there is enough space to move the block
-            # Insert the block at the new position and remove it from the old position
+        if start2 - end1 >= space_needed:  
             blocks.insert(i + 1, (to_move, end1, end1 + space_needed))
             blocks.remove(block)
             break
 
-# Calculate sum of `block_id * index` for all blocks
 print(
     sum(
         block_id * index
